844388bf7e76781f85a8492c5e4bb409610138cd
Raduan <rauan3002@gmail.com>
2024-06-16 23:16:28 +0200

    update: scraper.py performing a query from user's input

--- scripts/python/scraper.py
+++ scripts/python/scraper.py
@@ -5,7 +5,7 @@ from selenium.webdriver import Keys
 
 from time import sleep
 
-from json import dump
+from json import dump, loads
 
 # Fix auto closing browser
 options = webdriver.ChromeOptions()
@@ -13,12 +13,18 @@ options.add_experimental_option("detach", True)
 
 # setup
 driver = webdriver.Chrome(options=options)
-target = 'www.sberbank.ru'
+
+meta_file = open('scripts/data/meta.json', 'r')
+while not meta_file:
+    meta_file.close()
+    meta_file = open('scripts/data/meta.json', 'r')
+content = loads(meta_file.read())
+target = content['url']
 if target[:5] != 'https': 
     target = 'https://' + target
-filetype = 'pdf'
-location = 'intitle'
-phrase = 'Акции'
+filetype = content['format']
+location = content['option']
+phrase = content['phrase']
 
 
 actions = ActionChains(driver)
@@ -38,11 +44,11 @@ def get_google():
     #input the query
     input_field = driver.find_element(By.TAG_NAME, 'textarea')
     if not phrase:
-        input_field.send_keys(f'{target} filetype:{filetype}')
+        input_field.send_keys(f'site:{target} filetype:{filetype}')
     elif location == "inwebpage":
-        input_field.send_keys(f'{target} filetype:{filetype} "{phrase}"')
+        input_field.send_keys(f'site:{target} filetype:{filetype} "{phrase}"')
     else:
-        input_field.send_keys(f'{target} filetype:{filetype} {location}:{phrase}')
+        input_field.send_keys(f'site:{target} filetype:{filetype} {location}:{phrase}')
     actions.key_down(Keys.ENTER)
     actions.perform()
 
@@ -50,29 +56,9 @@ def get_google():
 def get_links():
     global link_title
 
-    # # get serps non-empy titles
-    # title_list = list()
-    # title_list = driver.find_elements(By.TAG_NAME, 'h3')
-    # title_list = [title.text for title in title_list if title.text]
-    # print(title_list, len(title_list))
-
-    # #get links 
-
-    # links = driver.find_elements(By.TAG_NAME,'a')
-    # links_list = []
-    # for link in links:
-    #     if link.get_attribute('jsname') == 'UWckNb' and link.get_attribute('href')[-4:] == f'{filetype}':
-    #         print(link.get_attribute('href')[-4:])
-    #         links_list.append(link.get_attribute('href'))
-    # print(links_list, len(links_list))
-
-    # link_title = {links_list[i]: title_list[i] for i in range(len(title_list))}
-    # print(link_title)
-    # '(By.xpath("./child::*"))'
     link_title = dict()
     title_list = driver.find_elements(By.CLASS_NAME, 'MjjYud')
-    # key = title_list[0].find_elements(By.TAG_NAME, 'a')
-    # print(key[0].get_attribute('href'), len(key))
+
     for container in title_list:
         key = container.find_elements(By.TAG_NAME, 'a')
         if len(key) != 1:
